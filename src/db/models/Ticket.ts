ce546684d2166533fd8c61014df97dbdf84bbcbc
Niemand <niemand@ymir-graphics.com>
2020-08-18 11:33:28 +0200

    create/open/close tickets

--- src/db/models/Ticket.ts
+++ src/db/models/Ticket.ts
@@ -1,11 +1,13 @@
 import mongoose from 'ts-mongoose';
 
+// const AuthorSchema = mongoose.createSchema({
+//     username: mongoose.Type.string({ required: true }),
+//     discriminator: mongoose.Type.string({ required: true }),
+//     avatar: mongoose.Type.string({ required: true }),
+// });
+
 const CommentSchema = mongoose.createSchema({
-    author: {
-        username: mongoose.Type.string({ required: true }),
-        discriminator: mongoose.Type.string({ required: true }),
-        avatar: mongoose.Type.string({ required: true }),
-    },
+    author: mongoose.Type.string({ required: true }),
     content: mongoose.Type.string({ required: true }),
     timestamp: mongoose.Type.string({ required: true }),
 });
@@ -15,8 +17,9 @@ const TicketSchema = mongoose.createSchema({
     title: mongoose.Type.string({ required: true }),
     topic: mongoose.Type.string({ required: true }),
     description: mongoose.Type.string({ required: true }),
-    closed: mongoose.Type.boolean({ required: true }),
+    author: mongoose.Type.string({ required: true }), // fetch user object (client side?) using https://discord.com/developers/docs/resources/user#get-user
     timestamp: mongoose.Type.string({ required: true }),
+    closed: mongoose.Type.boolean({ required: true }),
     comments: mongoose.Type.array().of(CommentSchema),
 });
 
