7c63f48c7cb83e323f43b2247011a747f6c56d3a
Niemand <niemand@ymir-graphics.com>
2020-08-21 16:31:42 +0200

    modticket instead of ticket for moderation

--- src/misc/searchMessage.ts
+++ src/misc/searchMessage.ts
@@ -23,39 +23,44 @@ export async function getUser(message: Message, potentialUser: string): Promise<
     return user;
 }
 
-export async function getTicket(message: Message, args: string[], text: string): Promise<ExtractDoc<typeof TicketSchema>> {
+export async function getTicket(message: Message, args: string[], text: string, closed: boolean | undefined): Promise<ExtractDoc<typeof TicketSchema>> {
     const { member } = message;
     if (!member) return Promise.reject();
     const id = args[0];
 
     let ticket;
     if (mongoose.Types.ObjectId.isValid(id)) {
-        if (member.permissions.has('MANAGE_MESSAGES')) {
-            ticket = await Ticket.findById(id);
-        } else {
-            const projects: string[] = [];
-            (await Project.find({ owners: member.id })).forEach((project) => projects.push(project.channel));
+        const projects: string[] = [];
+        (await Project.find({ owners: member.id })).forEach((project) => projects.push(project.channel));
 
+        if (closed === undefined) {
             ticket = await Ticket.findOne({ $and: [{ _id: id }, { $or: [{ author: message.author.id }, { project: { $in: projects } }] }] });
+        } else {
+            ticket = await Ticket.findOne({
+                $and: [{ _id: id }, { closed }, { $or: [{ author: message.author.id }, { project: { $in: projects } }] }],
+            });
         }
 
         if (!ticket) return Promise.reject(`There is no ticket with this ID that you have access to.`);
     } else {
-        if (member.permissions.has('MANAGE_MESSAGES')) {
-            ticket = await Ticket.findOne({ title: text });
-        } else {
-            const projects: string[] = [];
-            (await Project.find({ owners: member.id })).forEach((project) => projects.push(project.channel));
+        const projects: string[] = [];
+        (await Project.find({ owners: member.id })).forEach((project) => projects.push(project.channel));
 
+        if (closed === undefined) {
             ticket = await Ticket.findOne({
                 $and: [{ title: text }, { $or: [{ author: message.author.id }, { project: { $in: projects } }] }],
+            });
+        } else {
+            ticket = await Ticket.findOne({
+                $and: [{ title: text }, { closed }, { $or: [{ author: message.author.id }, { project: { $in: projects } }] }],
             });
         }
 
         if (!ticket) {
             let allTickets;
-            if (member.permissions.has('MANAGE_MESSAGES')) allTickets = await Ticket.find();
-            else allTickets = await Ticket.find({ author: message.author.id });
+
+            if (closed === undefined) allTickets = await Ticket.find({ author: message.author.id });
+            else allTickets = await Ticket.find({ $and: [{ closed }, { author: message.author.id }] });
 
             if (allTickets.length === 0) return Promise.reject(`There are no tickets that you have access to.`);
 
@@ -70,92 +75,26 @@ export async function getTicket(message: Message, args: string[], text: string):
     return ticket;
 }
 
-export async function getClosedTicket(message: Message, args: string[], text: string): Promise<ExtractDoc<typeof TicketSchema>> {
+export async function getTicketMod(message: Message, args: string[], text: string, closed: boolean | undefined): Promise<ExtractDoc<typeof TicketSchema>> {
     const { member } = message;
     if (!member) return Promise.reject();
     const id = args[0];
 
     let ticket;
     if (mongoose.Types.ObjectId.isValid(id)) {
-        if (member.permissions.has('MANAGE_MESSAGES')) {
-            ticket = await Ticket.findOne({ $and: [{ _id: id }, { closed: true }] });
-        } else {
-            const projects: string[] = [];
-            (await Project.find({ owners: member.id })).forEach((project) => projects.push(project.channel));
+        ticket = await Ticket.findById(id);
 
-            ticket = await Ticket.findOne({
-                $and: [{ _id: id }, { closed: true }, { $or: [{ author: message.author.id }, { project: { $in: projects } }] }],
-            });
-        }
-
-        if (!ticket) return Promise.reject(`There is no closed ticket with this ID that you have access to.`);
+        if (!ticket) return Promise.reject(`There is no ticket with this ID.`);
     } else {
-        if (member.permissions.has('MANAGE_MESSAGES')) {
-            ticket = await Ticket.findOne({ $and: [{ title: text }, { closed: true }] });
-        } else {
-            const projects: string[] = [];
-            (await Project.find({ owners: member.id })).forEach((project) => projects.push(project.channel));
-
-            ticket = await Ticket.findOne({
-                $and: [{ title: text }, { closed: true }, { $or: [{ author: message.author.id }, { project: { $in: projects } }] }],
-            });
-        }
+        ticket = await Ticket.findOne({ title: text });
 
         if (!ticket) {
             let allTickets;
-            if (member.permissions.has('MANAGE_MESSAGES')) allTickets = await Ticket.find({ closed: true });
-            else allTickets = await Ticket.find({ $and: [{ closed: true }, { author: message.author.id }] });
 
-            if (allTickets.length === 0) return Promise.reject(`There are no closed tickets that you have access to.`);
+            if (closed === undefined) allTickets = await Ticket.find();
+            else allTickets = await Ticket.find({ closed });
 
-            allTickets.sort((a, b) => {
-                return similarityLevenshtein(b.title, text) - similarityLevenshtein(a.title, text);
-            });
-
-            return Promise.reject(`No results found for "${text}". Did you mean "${allTickets[0].title}"?`);
-        }
-    }
-
-    return ticket;
-}
-
-export async function getOpenTicket(message: Message, args: string[], text: string): Promise<ExtractDoc<typeof TicketSchema>> {
-    const { member } = message;
-    if (!member) return Promise.reject();
-    const id = args[0];
-
-    let ticket;
-    if (mongoose.Types.ObjectId.isValid(id)) {
-        if (member.permissions.has('MANAGE_MESSAGES')) {
-            ticket = await Ticket.findOne({ $and: [{ _id: id }, { closed: false }] });
-        } else {
-            const projects: string[] = [];
-            (await Project.find({ owners: member.id })).forEach((project) => projects.push(project.channel));
-
-            ticket = await Ticket.findOne({
-                $and: [{ _id: id }, { closed: false }, { $or: [{ author: message.author.id }, { project: { $in: projects } }] }],
-            });
-        }
-
-        if (!ticket) return Promise.reject(`There is no open ticket with this ID that you have access to.`);
-    } else {
-        if (member.permissions.has('MANAGE_MESSAGES')) {
-            ticket = await Ticket.findOne({ $and: [{ title: text }, { closed: false }] });
-        } else {
-            const projects: string[] = [];
-            (await Project.find({ owners: member.id })).forEach((project) => projects.push(project.channel));
-
-            ticket = await Ticket.findOne({
-                $and: [{ title: text }, { closed: false }, { $or: [{ author: message.author.id }, { project: { $in: projects } }] }],
-            });
-        }
-
-        if (!ticket) {
-            let allTickets;
-            if (member.permissions.has('MANAGE_MESSAGES')) allTickets = await Ticket.find({ closed: false });
-            else allTickets = await Ticket.find({ $and: [{ closed: false }, { author: message.author.id }] });
-
-            if (allTickets.length === 0) return Promise.reject(`There are no open tickets that you have access to.`);
+            if (allTickets.length === 0) return Promise.reject(`There are no existing tickets.`);
 
             allTickets.sort((a, b) => {
                 return similarityLevenshtein(b.title, text) - similarityLevenshtein(a.title, text);
