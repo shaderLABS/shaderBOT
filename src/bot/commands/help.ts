7e650af27947caa7e5c1d4742b42ae817b068100
Niemand <niemand@ymir-graphics.com>
2020-08-30 22:24:17 +0200

    embed pages and other stuff

--- src/bot/commands/help.ts
+++ src/bot/commands/help.ts
@@ -1,5 +1,5 @@
 import { Command, hasPermissions } from '../commandHandler.js';
-import { sendInfo } from '../lib/embeds.js';
+import { sendInfo, embedPages } from '../lib/embeds.js';
 import { settings, commands } from '../bot.js';
 import { Collection } from 'discord.js';
 
@@ -8,9 +8,11 @@ export const command: Command = {
     help: 'Show this help page.',
     minArgs: 0,
     maxArgs: 0,
-    callback: (message) => {
+    callback: async (message) => {
+        let pages: string[] = [];
         let helpContent = '';
-        commands.forEach((value, key) => {
+        let index = 0;
+        for (var [key, value] of commands) {
             if (value instanceof Collection) {
                 let subCmdHelp = '';
 
@@ -18,12 +20,26 @@ export const command: Command = {
                     if (hasPermissions(message, subValue)) subCmdHelp += `\n• \`${JSON.parse(subKey).join('|')}\` - ${subValue.help}`;
                 });
 
-                if (subCmdHelp !== '') helpContent += `\`${settings.prefix}${JSON.parse(key).join('|')}\`${subCmdHelp}\n\n`;
+                if (subCmdHelp !== '') {
+                    helpContent += `\`${settings.prefix}${JSON.parse(key).join('|')}\`${subCmdHelp}\n\n`;
+                    index++;
+                }
             } else {
-                if (hasPermissions(message, value)) helpContent += `\`${settings.prefix}${JSON.parse(key).join('|')}\`\n${value.help}\n\n`;
+                if (hasPermissions(message, value)) {
+                    helpContent += `\`${settings.prefix}${JSON.parse(key).join('|')}\`\n${value.help}\n\n`;
+                    index++;
+                }
             }
-        });
 
-        sendInfo(message.channel, helpContent, 'HELP');
+            if (index === 3) {
+                index = 0;
+                pages.push(helpContent);
+                helpContent = '';
+            }
+        }
+
+        pages.push(helpContent);
+
+        embedPages(await sendInfo(message.channel, pages[0], 'HELP'), message.author, pages);
     },
 };
--- src/bot/commands/modticket/open.ts
+++ src/bot/commands/modticket/open.ts
@@ -1,7 +1,7 @@
 import { Command } from '../../commandHandler.js';
 import { Message } from 'discord.js';
 import { getTicketMod } from '../../lib/searchMessage.js';
-import { sendSuccess, sendError } from '../../lib/embeds.js';
+import { sendSuccess, sendError, sendInfo } from '../../lib/embeds.js';
 import log from '../../lib/log.js';
 import { openTicket } from '../../lib/tickets.js';
 
@@ -11,20 +11,23 @@ export const command: Command = {
     expectedArgs: '<ticketID|ticketTitle>',
     minArgs: 1,
     maxArgs: null,
-    superCommands: ['modticket'],
+    superCommands: ['modticket', 'mticket'],
     requiredPermissions: ['MANAGE_MESSAGES'],
     callback: async (message: Message, args: string[], text: string) => {
-        const { guild } = message;
+        const { guild, channel } = message;
         if (!guild) return;
 
         try {
             let ticket = await getTicketMod(message, args, text, true);
-            openTicket(ticket, guild);
 
-            sendSuccess(message.channel, 'Ticket opened.');
+            const loadingEmbed = await sendInfo(channel, 'Opening ticket...');
+            await openTicket(ticket, guild);
+            await loadingEmbed.delete();
+
+            sendSuccess(channel, 'Ticket opened.');
             log(`<@${message.author.id}> opened the ticket "${ticket.title}" by <@${ticket.author}>.`);
         } catch (error) {
-            if (error) sendError(message.channel, error);
+            if (error) sendError(channel, error);
         }
     },
 };
