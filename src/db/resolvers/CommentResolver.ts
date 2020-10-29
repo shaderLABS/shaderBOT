2e6ad7629769262cbc400aa636836f6c848b2615
Niemand <niemand@ymir-graphics.com>
2020-10-29 14:05:24 +0100

    edit comments

--- src/db/resolvers/CommentResolver.ts
+++ src/db/resolvers/CommentResolver.ts
@@ -1,4 +1,4 @@
-import { Permissions, TextChannel } from 'discord.js';
+import { MessageEmbed, Permissions, TextChannel } from 'discord.js';
 import tgq from 'type-graphql';
 import gq from 'graphql';
 import { client } from '../../bot/bot.js';
@@ -41,6 +41,109 @@ export class CommentResolver {
         log(`<@${ctx.req.user.id}> deleted ticket comment by <@${deleted.rows[0].author_id}>:\n\n${deleted.rows[0].content}`);
 
         return deleted.rows[0];
+    }
+
+    @tgq.Mutation(() => Boolean)
+    async createComment(
+        @tgq.Arg('ticket_id', () => String) ticket_id: string,
+        @tgq.Arg('content', () => String) content: string,
+        @tgq.Ctx() ctx: any
+    ) {
+        const user = ctx.req.user;
+        if (!user) return new gq.GraphQLError('You must be logged in to send comments.');
+
+        if (!content) return new gq.GraphQLError('The content may not be empty.');
+        if (content.length > 2000) return new gq.GraphQLError('The content may not be over 2000 characters long.');
+
+        const ticket = (
+            await db.query(
+                /*sql*/ `
+                SELECT id, title, project_channel_id::TEXT, channel_id::TEXT, description, attachments, author_id::TEXT, timestamp::TEXT, edited::TEXT, closed 
+                FROM ticket
+                WHERE id = $1 AND closed = FALSE
+                LIMIT 1;`,
+                [ticket_id]
+            )
+        ).rows[0];
+
+        if (!ticket) return new gq.GraphQLError('The ticket does not exist or is closed.');
+
+        const channel = client.guilds.cache.first()?.channels.cache.get(ticket.channel_id);
+        if (!channel || !(channel instanceof TextChannel)) return new gq.GraphQLError('Ticket channel not found.');
+
+        const timestamp = new Date();
+
+        const commentEmbed = new MessageEmbed()
+            .setColor(user.roleColor || '#212121')
+            .setAuthor(user.username + '#' + user.discriminator, user.avatarURL || undefined)
+            .setTimestamp(timestamp)
+            .setDescription(content);
+
+        const commentMessage = await channel.send(commentEmbed);
+
+        await db.query(
+            /*sql*/ `
+            INSERT INTO comment (ticket_id, author_id, message_id, content, timestamp)
+            VALUES ($1, $2, $3, $4, $5)`,
+            [ticket_id, user.id, commentMessage.id, content, timestamp]
+        );
+
+        return true;
+    }
+
+    @tgq.Mutation(() => Boolean)
+    async editComment(
+        @tgq.Arg('comment_id', () => String) comment_id: string,
+        @tgq.Arg('content', () => String) content: string,
+        @tgq.Ctx() ctx: any
+    ) {
+        const user = ctx.req.user;
+        if (!user) return new gq.GraphQLError('You must be logged in to edit comments.');
+
+        const comment = (
+            await db.query(
+                /*sql*/ `
+            SELECT ticket.channel_id, comment.content, comment.timestamp, comment.message_id
+            FROM comment
+            INNER JOIN ticket ON ticket.id = comment.ticket_id
+            WHERE comment.id = $1 AND comment.author_id = $2
+            LIMIT 1;
+        `,
+                [comment_id, user.id]
+            )
+        ).rows[0];
+
+        if (!comment) return new gq.GraphQLError('The comment does not exist or you have no permission to edit it.');
+
+        const guild = client.guilds.cache.first();
+        if (!guild) return new gq.GraphQLError('No guild found.');
+
+        const editedAt = new Date();
+
+        const channel = guild.channels.cache.get(comment.channel_id);
+        if (channel && channel instanceof TextChannel) {
+            const originalMessage = await channel.messages.fetch(comment.message_id);
+            if (originalMessage) {
+                const embed = originalMessage.embeds[0];
+                if (embed) {
+                    embed.setFooter(`edited at ${editedAt.toLocaleString()}`);
+                    embed.setDescription(content);
+
+                    await originalMessage.edit(embed);
+                }
+            }
+        }
+
+        await db.query(
+            /*sql*/ `
+            UPDATE comment 
+            SET content = $1, edited = $2 
+            WHERE id = $3`,
+            [content, editedAt, comment_id]
+        );
+
+        log(`<@${user.id}> edited their ticket comment from:\n\n${comment.content}\n\nto:\n\n${content}`);
+        return true;
     }
 
     // @tgq.FieldResolver({ name: 'ticket', nullable: true })
