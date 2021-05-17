27e5dd2f4617886da6351f9b88bde180cab90ec7
Kneemund <niemand@ymir-graphics.com>
2021-05-17 20:59:39 +0200

    feat: specify websocket intents (-> less data)

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -13,6 +13,7 @@ export let commands: Collection<string, Command | Collection<string, Command>>;
 export let events: Collection<string, Event>;
 export let pastas: Collection<string, Pasta>;
 export let autoResponses: Collection<string, AutoResponse>;
+export let cooldowns: Map<string, boolean>;
 export let settings: settingsFile.Settings;
 
 cron.schedule('55 23 * * *', () => {
@@ -21,11 +22,18 @@ cron.schedule('55 23 * * *', () => {
 });
 
 export async function startBot() {
-    client = new Client({ disableMentions: 'everyone', partials: ['MESSAGE', 'REACTION', 'GUILD_MEMBER'], messageEditHistoryMaxSize: 0 });
+    client = new Client({
+        disableMentions: 'everyone',
+        partials: ['MESSAGE', 'REACTION', 'GUILD_MEMBER'],
+        messageEditHistoryMaxSize: 0,
+        ws: { intents: ['GUILDS', 'GUILD_MEMBERS', 'GUILD_BANS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS', 'DIRECT_MESSAGES'] },
+    });
+
     commands = new Collection<string, Command>();
     events = new Collection<string, Event>();
     pastas = new Collection<string, Pasta>();
     autoResponses = new Collection<string, AutoResponse>();
+    cooldowns = new Map<string, boolean>();
     settings = await settingsFile.read();
 
     registerCommands('./src/bot/commands').then(() => console.debug(`Registered Commands:\n\t${commandsToDebugMessage(commands)}`));
