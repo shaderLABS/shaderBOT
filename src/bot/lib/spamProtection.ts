ffc414a30b1e5fd916d1ba3564b1e95f0ff2358f
Kneemund <niemand@ymir-graphics.com>
2021-08-02 16:59:36 +0200

    fix: potential race condition in spam filter

--- src/bot/lib/spamProtection.ts
+++ src/bot/lib/spamProtection.ts
@@ -18,14 +18,22 @@ const cache: (cachedMessage | undefined)[] = new Array(settings.spamProtection.c
 export function checkSpam(message: GuildMessage) {
     if (message.attachments.size || message.content.length < settings.spamProtection.characterThreshold || !message.content.includes('http')) return false;
 
+    const currentMessage = {
+        id: message.id,
+        content: message.content,
+        authorID: message.author.id,
+        channelID: message.channel.id,
+        createdTimestamp: message.createdTimestamp,
+    };
+
     const potentialSpam = cache.filter(
         (previousMessage) =>
             previousMessage &&
-            message.author.id === previousMessage.authorID &&
-            message.content === previousMessage.content &&
-            message.channel.id !== previousMessage.channelID &&
-            message.createdTimestamp - previousMessage.createdTimestamp < settings.spamProtection.timeThreshold * 1000
-    ) as cachedMessage[];
+            currentMessage.authorID === previousMessage.authorID &&
+            currentMessage.content === previousMessage.content &&
+            currentMessage.channelID !== previousMessage.channelID &&
+            currentMessage.createdTimestamp - previousMessage.createdTimestamp < settings.spamProtection.timeThreshold * 1000
+    );
 
     const isSpam = potentialSpam.length >= settings.spamProtection.messageThreshold - 1;
     if (isSpam) {
@@ -33,17 +41,20 @@ export function checkSpam(message: GuildMessage) {
             log(`Failed to mute ${parseUser(message.author)} due to spam: ${e}`)
         );
 
+        const spamMessages = cache.filter((previousMessage) => previousMessage && message.author.id === previousMessage.authorID) as cachedMessage[];
+        const guild = message.guild;
+
         message.delete().catch(() => undefined);
-        potentialSpam.forEach(async (spam) => {
-            const spamChannel = message.guild.channels.cache.get(spam.channelID);
+
+        for (const spam of spamMessages) {
+            const spamChannel = guild.channels.cache.get(spam.channelID);
             if (spamChannel && spamChannel instanceof TextChannel) {
-                const spamMessage = await spamChannel.messages.fetch(spam.id).catch(() => undefined);
-                if (spamMessage) spamMessage.delete().catch(() => undefined);
+                spamChannel.messages.delete(spam.id).catch(() => undefined);
             }
-        });
+        }
     }
 
-    cache.unshift({ id: message.id, content: message.content, authorID: message.author.id, channelID: message.channel.id, createdTimestamp: message.createdTimestamp });
+    cache.unshift(currentMessage);
     cache.pop();
 
     return isSpam;
