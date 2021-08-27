6ac3b9f5dbb8e7f3ecdf5f0f215b9b464555094e
Kneemund <niemand@ymir-graphics.com>
2021-08-27 20:44:35 +0200

    feat: selection menus for !backup list

--- src/bot/commands/backup/list.ts
+++ src/bot/commands/backup/list.ts
@@ -1,20 +1,20 @@
-import { MessageAttachment } from 'discord.js';
+import { MessageActionRow, MessageAttachment, MessageSelectMenu } from 'discord.js';
 import fs from 'fs/promises';
 import path from 'path';
 import { Command } from '../../commandHandler.js';
 import { backupPath, readBackup } from '../../lib/backup.js';
 import { sendError, sendInfo } from '../../lib/embeds.js';
+import { formatTimeDate } from '../../lib/time.js';
 
 export const command: Command = {
     commands: ['list'],
     superCommands: ['backup'],
-    help: 'List all local backups or decrypt & send a specific one.',
-    expectedArgs: '[index]',
+    help: 'Select, decrypt & send a backup.',
     minArgs: 0,
-    maxArgs: 1,
+    maxArgs: 0,
     requiredPermissions: ['MANAGE_MESSAGES'],
-    callback: async (message, args) => {
-        const { channel } = message;
+    callback: async (message) => {
+        const { author, channel } = message;
 
         // read backup dir & sort by creation time
         const files: string[] = await fs.readdir(backupPath).catch((error) => {
@@ -32,16 +32,29 @@ export const command: Command = {
             )
         ).sort((a, b) => b.createdAt - a.createdAt);
 
-        if (!args[0]) {
-            const content = backups.reduce((prev, curr, i) => {
-                return prev + `\`${i + 1}\` - ${curr.name}\n`;
-            }, '');
+        const menu = new MessageSelectMenu({
+            customId: 'select-backup',
+            placeholder: backups.length + ' backups available...',
+            options: backups.map((backup, index) => {
+                return {
+                    label: backup.name.substring(0, backup.name.lastIndexOf(' - ')),
+                    value: index.toString(),
+                    description: formatTimeDate(new Date(backup.createdAt)),
+                };
+            }),
+        });
 
-            sendInfo(channel, content, 'Backups');
-        } else {
-            const index = +args[0];
-            if (index < 1 || index > backups.length) return sendError(channel, 'Index out of bounds.');
-            const backup = backups[index - 1];
+        const selectionMessage = await channel.send({ content: '**Select a Backup**', components: [new MessageActionRow({ components: [menu] })] });
+        const selection = await selectionMessage
+            .awaitMessageComponent({
+                componentType: 'SELECT_MENU',
+                filter: (interaction) => interaction.user.id === author.id,
+                time: 300000,
+            })
+            .catch(() => undefined);
+
+        if (selection?.isSelectMenu()) {
+            const backup = backups[+selection.values[0]];
 
             try {
                 const data = await readBackup(backup.name);
@@ -49,6 +62,10 @@ export const command: Command = {
             } catch (error) {
                 sendError(channel, error);
             }
+
+            selection.update({ components: [new MessageActionRow({ components: [menu.setPlaceholder(backup.name).setDisabled(true)] })] });
+        } else {
+            selectionMessage.edit({ components: [new MessageActionRow({ components: [menu.setDisabled(true)] })] });
         }
     },
 };
