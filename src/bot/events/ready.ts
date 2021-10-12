f369cf17665f990c05471d9545bc58fa8e2e5299
Kneemund <niemand@ymir-graphics.com>
2021-10-12 19:56:04 +0200

    fix: load slash commands

--- src/bot/events/ready.ts
+++ src/bot/events/ready.ts
@@ -1,6 +1,8 @@
 import { client } from '../bot.js';
 import { Event } from '../eventHandler.js';
+import { getGuild } from '../lib/misc.js';
 import { loadTimeouts } from '../lib/punishments.js';
+import { registerSlashCommands } from '../slashCommandHandler.js';
 
 export const event: Event = {
     name: 'ready',
@@ -9,5 +11,9 @@ export const event: Event = {
         console.log(`Logged in as '${client.user.tag}'.`);
 
         loadTimeouts(false);
+
+        const guild = getGuild();
+        if (!guild) return console.error('Failed to load slash commands: the specified guild was not found.');
+        await registerSlashCommands('./src/bot/slashCommands');
     },
 };
