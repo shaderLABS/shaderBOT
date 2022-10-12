67693e8cf6972c214c6f65b5666e04d0ff2cf64c
Kneemund <niemand@ymir-graphics.com>
2022-10-12 18:35:07 +0200

    fix: update & improve PostgreSQL batch scripts

--- scripts/dev/stop_postgres.bat
+++ scripts/dev/stop_postgres.bat
@@ -1,4 +1,7 @@
-@echo off
-if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
-
-net stop postgresql-x64-13
+for /F "tokens=3 delims=: " %%H in ('sc query "postgresql-x64-14" ^| findstr "        STATE"') do (
+  if /I "%%H" EQU "RUNNING" (
+    @echo off
+    if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
+    net stop postgresql-x64-14
+  )
+)
