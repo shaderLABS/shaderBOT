71d04b7091fbf431975335b6b8c475cfbc92fcab
Kneemund <niemand@ymir-graphics.com>
2025-03-17 16:46:00 +0100

    fix: remove imports from dev config & version override

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,20 +1,9 @@
 import { defineConfig } from 'drizzle-kit';
-import type { PoolConfig } from 'pg';
-
-export const DB_CREDENTIALS = {
-    ssl: false,
-    user: process.env.PG_USER || 'postgres',
-    password: process.env.PG_PASSWORD || 'postgres',
-    host: process.env.PG_HOST || 'localhost',
-    port: process.env.PG_PORT ? Number.parseInt(process.env.PG_PORT) : 5432,
-    database: process.env.PG_DATABASE || 'shaderBOT',
-} satisfies PoolConfig;
-
-export const DRIZZLE_OUTPUT = './drizzle';
+import { DB_CREDENTIALS, DB_DRIZZLE_OUTPUT } from './src/db/postgres.ts';
 
 export default defineConfig({
     dialect: 'postgresql',
     schema: './src/db/schema.ts',
-    out: DRIZZLE_OUTPUT,
+    out: DB_DRIZZLE_OUTPUT,
     dbCredentials: DB_CREDENTIALS,
 });
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import { migrate } from 'drizzle-orm/node-postgres/migrator';
-import { DRIZZLE_OUTPUT } from '../drizzle.config.ts';
 import { startBot } from './bot/bot.ts';
-import { db } from './db/postgres.ts';
+import { db, DB_DRIZZLE_OUTPUT } from './db/postgres.ts';
 import { startWebserver } from './web/server.ts';
 
 console.log('Migrating database...');
-await migrate(db, { migrationsFolder: DRIZZLE_OUTPUT });
+await migrate(db, { migrationsFolder: DB_DRIZZLE_OUTPUT });
 
 startBot();
 startWebserver();
